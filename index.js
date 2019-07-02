1042182d9d51fe4ee121174b3a889d50831750b0
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-07-02 23:05:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested.target')
+}
+
+function increaseRankBy(n){
+  let parsed = document.querySelectorAll('ul.ranked-list li')
+  for(let i = 0; i < parsed.length; i++){
+    parsed[i].innerHTML = parseInt(parsed[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  let tester;
+  let base = document.querySelector('#grand-node').querySelectorAll('div')
+  for(let i = 0; i < base.length; i++){
+    tester = base[i]
+  }
+}
